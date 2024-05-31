076d0dd42e74366e4d6c45986e660c5b2e03d5f7
Fran-Lis <fl66@live.it>
2024-05-31 19:23:58 +0200

    modified:   app.ts

--- app.js
+++ app.js
@@ -38,3 +38,23 @@ var Company = /** @class */ (function () {
     };
     return Company;
 }());
+// Examples
+var participant1 = new Participant('Mario', 'Rossi', 'Italia', 'Diploma di maturità scientifica', ['Italiano', 'Inglese'], 'artigianato', []);
+var participant2 = new Participant('Marco', 'Verdi', 'Italia', 'Diploma di maturità classica', ['Italiano', 'Spagnolo'], 'artigianato', []);
+var participant3 = new Participant('Anna', 'Neri', 'Italia', 'Diploma di maturità linguistica', ['Italiano', 'Inglese', 'Spagnolo', 'Tedesco'], 'ristorazione', []);
+var course1 = new Course('Corso per intagliatori di legname', 'Ti insegnamo a intagliare il legno', 135, 'artigianato', []);
+var course2 = new Course('Corso per cuochi', 'Ti insegnamo a cucinare', 150, 'ristorazione', []);
+var company1 = new Company('Artigiani4Ever', 'artigianato', 'Abbiamo aperto più di 100 botteghe in Italia', ['Intagliatore di legname', 'Fabbro']);
+var company2 = new Company('PizzaSud', 'artigianato', 'Abbiamo aperto più di 30 pizzerie nel Sud Italia', ['Pizzaiolo', 'Cuoco']);
+participant1.subscribeCourse(course1);
+course1.addSubscriber(participant2);
+console.log("course1 subscribers: ".concat(course1.subscribers.map(function (el) { return " ".concat(el.firstName, " ").concat(el.lastName); })));
+participant3.subscribeCourse(course2);
+console.log("course2 subscribers: ".concat(course2.subscribers.map(function (el) { return " ".concat(el.firstName, " ").concat(el.lastName); })));
+company1.offerPosition(participant1, company1.openPositions[0]);
+company1.offerPosition(participant2, company1.openPositions[0]);
+company1.offerPosition(participant2, company1.openPositions[1]);
+company2.offerPosition(participant3, company2.openPositions[1]);
+console.log("offers for participant1: ".concat(participant1.offers));
+console.log("offers for participant2: ".concat(participant2.offers));
+console.log("offers for participant3: ".concat(participant3.offers));
--- app.ts
+++ app.ts
@@ -67,3 +67,31 @@ class Company implements ICompany{
         participant.offers.push(`${position} by ${this.name}`)
     }
 }
+
+// Examples
+
+let participant1 = new Participant('Mario', 'Rossi', 'Italia', 'Diploma di maturità scientifica', ['Italiano', 'Inglese'], 'artigianato', [])
+let participant2 = new Participant('Marco', 'Verdi', 'Italia', 'Diploma di maturità classica', ['Italiano', 'Spagnolo'], 'artigianato', [])
+let participant3 = new Participant('Anna', 'Neri', 'Italia', 'Diploma di maturità linguistica', ['Italiano', 'Inglese', 'Spagnolo', 'Tedesco'], 'ristorazione', [])
+
+let course1 = new Course('Corso per intagliatori di legname', 'Ti insegnamo a intagliare il legno', 135, 'artigianato', [])
+let course2 = new Course('Corso per cuochi', 'Ti insegnamo a cucinare', 150, 'ristorazione', [])
+
+let company1 = new Company('Artigiani4Ever', 'artigianato', 'Abbiamo aperto più di 100 botteghe in Italia', ['Intagliatore di legname', 'Fabbro'])
+let company2 = new Company('PizzaSud', 'artigianato', 'Abbiamo aperto più di 30 pizzerie nel Sud Italia', ['Pizzaiolo', 'Cuoco'])
+
+participant1.subscribeCourse(course1)
+course1.addSubscriber(participant2)
+console.log(`course1 subscribers: ${course1.subscribers.map((el) => {return ` ${el.firstName} ${el.lastName}`})}`)
+
+participant3.subscribeCourse(course2)
+console.log(`course2 subscribers: ${course2.subscribers.map((el) => {return ` ${el.firstName} ${el.lastName}`})}`)
+
+company1.offerPosition(participant1, company1.openPositions[0])
+company1.offerPosition(participant2, company1.openPositions[0])
+company1.offerPosition(participant2, company1.openPositions[1])
+company2.offerPosition(participant3, company2.openPositions[1])
+
+console.log(`offers for participant1: ${participant1.offers}`)
+console.log(`offers for participant2: ${participant2.offers}`)
+console.log(`offers for participant3: ${participant3.offers}`)
